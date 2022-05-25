9ed219b715260bd219ee6641e7e679bc9e3c3645
BlagoKolev <blagovest.kolev@gmail.com>
2022-05-25 11:59:21 +0300

    Add two solved problems.

---
+++ 1.1.PlusMinus/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<int> arr)
+    {
+        decimal arrLength = arr.Count;
+        decimal positive = 0;
+        decimal negative = 0;
+        decimal zeros = 0;
+
+        for (int i = 0; i < arrLength; i++)
+        {
+
+            if (arr[i] > 0)
+            {
+                positive++;
+            }
+            else if (arr[i] < 0)
+            {
+                negative++;
+            }
+            else
+            {
+                zeros++;
+            }
+        }
+        Console.WriteLine((positive / arrLength).ToString("N6"));
+        Console.WriteLine((negative / arrLength).ToString("N6"));
+        Console.WriteLine((zeros / arrLength).ToString("N6"));
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.plusMinus(arr);
+    }
+}
---
+++ 1.2.Mini-MaxSum/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+using System.Numerics;
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<long> arr)
+    {
+
+        var resultArr = new List<BigInteger>();
+
+        for (int i = 0; i < arr.Count; i++)
+        {
+            BigInteger sum = (arr[0] + arr[1] + arr[2] + arr[3] + arr[4]) - arr[i];
+            resultArr.Add(sum);
+        }
+        Console.WriteLine($"{resultArr.Min()} {resultArr.Max()}");
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<long> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt64(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
