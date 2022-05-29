53b845719f380dbe912e95939ec251579bef361f
BlagoKolev <blagovest.kolev@gmail.com>
2022-05-29 22:48:31 +0300

    Add a new solution.

---
+++ 1.5.CountingSort1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'countingSort' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<int> countingSort(List<int> arr)
+    {
+        var resultArr = new int[100];
+        var counter = 1;
+
+        for (int i = 0; i < arr.Count; i++)
+        {
+            var currerntNum = arr[i];
+            if (resultArr[currerntNum] != 0)
+            {
+                continue;
+            }
+            for (int j = 0; j < arr.Count; j++)
+            {
+                var nextNum = arr[j];
+
+                if (i == j)
+                {
+                    continue;
+                }
+                if (currerntNum == nextNum)
+                {
+                    counter++;
+                }
+            }
+            resultArr[currerntNum] = counter;
+            counter = 1;
+        }
+        return resultArr.ToList();
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        List<int> result = Result.countingSort(arr);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
