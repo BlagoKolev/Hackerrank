83148887f255f1373ace2baa8fb26a168a679851
BlagoKolev <blagovest.kolev@gmail.com>
2022-06-02 15:41:57 +0300

    Add new problem solution.

---
+++ 1.6.TimeConversion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1._6.TimeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
