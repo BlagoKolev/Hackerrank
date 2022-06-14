c04b417c9a67e9c934dee8ae3f542c0558cd73fd
BlagoKolev <blagovest.kolev@gmail.com>
2022-06-14 06:43:24 +0300

    Add new solution.

---
+++ 2.4.Pangrams/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'pangrams' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string pangrams(string s)
+    {
+        var alphabet = new List<string>() { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", };
+        var alphaCheker = new List<string>();
+
+        foreach (var letter in s)
+        {
+            var letterString = letter.ToString();
+            if (alphabet.Contains(letterString.ToLower()) && !alphaCheker.Contains(letterString.ToLower()))
+            {
+                alphaCheker.Add(letterString.ToLower());
+            }
+        }
+        if (alphaCheker.Count == 26)
+        {
+            return "pangram";
+        }
+        return "not pangram";
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        string result = Result.pangrams(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
